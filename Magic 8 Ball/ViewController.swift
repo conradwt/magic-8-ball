1a4658958eeea1aa7389d6be7781c34d71d53dcb
Conrad Taylor <conradwt@gmail.com>
2019-01-21 17:34:17 -0800

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Conrad Taylor on 1/21/19.
+//  Copyright © 2019 Conrad Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
